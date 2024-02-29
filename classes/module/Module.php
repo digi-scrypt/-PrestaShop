881630a6895a75bb2d1ef7bd22590c9f64376637
PululuK <pululuandre@hotmail.com>
2024-02-29 13:01:24 +0100

    Implement entity schema manager in module

--- classes/module/Module.php
+++ classes/module/Module.php
@@ -36,6 +36,7 @@ use PrestaShop\PrestaShop\Core\Module\Legacy\ModuleInterface;
 use PrestaShop\PrestaShop\Core\Module\ModuleOverrideChecker;
 use PrestaShop\PrestaShop\Core\Module\WidgetInterface;
 use PrestaShop\PrestaShop\Core\Security\Permission;
+use PrestaShop\PrestaShop\Core\Util\Database\EntitySchemaManager;
 use PrestaShop\TranslationToolsBundle\Translation\Helper\DomainHelper;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException;
@@ -3699,6 +3700,16 @@ abstract class ModuleCore implements ModuleInterface
         static::$modules_cache = null;
         static::$cachedModuleNames = null;
         Cache::clean('Module::isEnabled*');
+    }
+
+    /**
+     * Return entity schema manager service
+     *
+     * @return EntitySchemaManager
+     */
+    public function getEntitySchemaManager(): EntitySchemaManager
+    {
+        return $this->get('prestashop.core.util.database.entity_cchema_manager');
     }
 }
 
