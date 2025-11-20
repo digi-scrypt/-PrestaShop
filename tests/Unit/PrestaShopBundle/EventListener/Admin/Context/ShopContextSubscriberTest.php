ff6d125ca821b4c5daf963a441a4874bd733cccf
David-Julian Buch <david@web-premiere.fr>
2025-11-21 00:36:59 +0100

    Change router mock to use RequestMatcherInterface

--- tests/Unit/PrestaShopBundle/EventListener/Admin/Context/ShopContextSubscriberTest.php
+++ tests/Unit/PrestaShopBundle/EventListener/Admin/Context/ShopContextSubscriberTest.php
@@ -41,7 +41,7 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Exception\NoConfigurationException;
-use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\Matcher\RequestMatcherInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Event\AuthenticationSuccessEvent;
@@ -544,9 +544,9 @@ class ShopContextSubscriberTest extends ContextEventListenerTestCase
         ];
     }
 
-    private function mockRouter(): Router|MockObject
+    private function mockRouter(): RequestMatcherInterface|MockObject
     {
-        $router = $this->createMock(Router::class);
+        $router = $this->createMock(RequestMatcherInterface::class);
         $router
             ->method('matchRequest')
             ->willThrowException(new NoConfigurationException())
