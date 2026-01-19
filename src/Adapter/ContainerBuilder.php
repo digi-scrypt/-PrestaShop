89a740a9e03963202601af8df2a06ba035a1cf9c
Benoît Gross <gross@novius.fr>
2026-01-19 14:17:04 +0100

    Add support for ServiceEntityRepository in FrontContainer

--- src/Adapter/ContainerBuilder.php
+++ src/Adapter/ContainerBuilder.php
@@ -26,6 +26,7 @@
 
 namespace PrestaShop\PrestaShop\Adapter;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\ServiceRepositoryCompilerPass;
 use Doctrine\Common\Cache\Psr6\DoctrineProvider;
 use Doctrine\ORM\Tools\Setup;
 use Exception;
@@ -186,6 +187,7 @@ class ContainerBuilder
 
         $container->addCompilerPass(new LoadServicesFromModulesPass($this->containerName), PassConfig::TYPE_BEFORE_OPTIMIZATION, PrestaShopBundle::LOAD_MODULE_SERVICES_PASS_PRIORITY);
         $container->addCompilerPass(new LegacyCompilerPass());
+        $container->addCompilerPass(new ServiceRepositoryCompilerPass());
 
         // Build extensions
         $builderExtensions = [
